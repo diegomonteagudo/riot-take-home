70fece8350885ed5505fa8427f5426c615feaf18
Diego Monteagudo <d.monteagudo13@gmail.com>
2025-11-25 08:59:39 +0100

    Add unencrypted attribute detection feature for /decrypt

--- app/core/encryption_strategies.py
+++ app/core/encryption_strategies.py
@@ -11,6 +11,10 @@ class EncryptionStrategy(ABC):
     def decrypt(self, value: str):
         pass
 
+    @abstractmethod
+    def is_encrypted(self, value: str) -> bool:
+        pass
+
     def encrypt_json_payload(self, payload: dict) -> dict:
         if not payload:
             return {}
@@ -29,7 +33,25 @@ class Base64EncryptionStrategy(EncryptionStrategy):
         return base64.b64encode(utf8_bytes).decode("utf-8")
     
     def decrypt(self, value: str):
-        base64_bytes = value.encode("utf-8")
-        utf8_bytes = base64.b64decode(base64_bytes)
-        serialized_json = utf8_bytes.decode("utf-8")
-        return json.loads(serialized_json)
+        if self.is_encrypted(value):
+            base64_bytes = value.encode("utf-8")
+            utf8_bytes = base64.b64decode(base64_bytes)
+            serialized_json = utf8_bytes.decode("utf-8")
+            return json.loads(serialized_json)
+        return value
+    
+    def is_encrypted(self, value: str) -> bool:
+        """ Check if the given value is a Base64-encoded JSON string. 
+        In its current version, it checks if an error is raised when attempting
+        to decode and parse the value.
+        Potential limitation: a non-encrypted string that happens to be a valid
+        Base64-encoded JSON string could be misidentified as encrypted.
+        """
+        try:
+            base64_bytes = value.encode("utf-8")
+            utf8_bytes = base64.b64decode(base64_bytes)
+            serialized_json = utf8_bytes.decode("utf-8")
+            json.loads(serialized_json)
+            return True
+        except (ValueError, json.JSONDecodeError, UnicodeDecodeError):
+            return False
--- tests/test_decryption.py
+++ tests/test_decryption.py
@@ -182,3 +182,32 @@ def test_encryption_then_decryption_nested_3_levels():
     decrypted_data = decrypt_response.json()
     assert decrypted_data == original_payload
 
+def test_already_unecrypted_value():
+    payload = {"name": "John Doe"}
+    response = client.post("/decrypt", json=payload)
+    assert response.status_code == 200
+    decrypted_data = response.json().get("name")
+    assert decrypted_data == "John Doe"
+
+
+def test_mixed_encrypted_and_unecrypted_values():
+    payload = {
+        "name": toBase64("John Doe"),
+        "age": toBase64(30),
+        "contact": toBase64({
+            "email": "john@example.com",
+            "phone": "123-456-7890"
+        }),
+        "birth_date": "1998-11-19"
+    }
+    response = client.post("/decrypt", json=payload)
+    assert response.status_code == 200
+    decrypted_data = response.json()
+    assert decrypted_data.get("name") == "John Doe"
+    assert decrypted_data.get("age") == 30
+    assert decrypted_data.get("contact") == {
+        "email": "john@example.com",
+        "phone": "123-456-7890"
+    }
+    assert decrypted_data.get("birth_date") == "1998-11-19"
+
