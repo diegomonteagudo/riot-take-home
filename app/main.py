cdd9544e697f99f67c1bfeebd29c1c1f93a21778
diegomonteagudo <83817733+diegomonteagudo@users.noreply.github.com>
2025-11-26 18:59:44 +0100

    Add docstrings and OpenAPI documentation

--- app/main.py
+++ app/main.py
@@ -2,7 +2,13 @@ from fastapi import FastAPI
 
 from app.endpoints import router
 
-app = FastAPI(title="Riot Take-Home Test")
+app = FastAPI(
+    title="Riot Take-Home Test",
+    description="An API to encrypt, decrypt, sign and verify JSON payloads.",
+    contact={
+        "name": "Diego Monteagudo",
+        "url": "https://github.com/diegomonteagudo/riot-take-home"}
+)
 
 app.include_router(router)
 
